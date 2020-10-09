b3d7988162b958d057a9a177bfbe677038c48dad
João <jmrosal@JMRosal-Mac-Book-Air.local>
2020-10-09 12:01:05 -0300

    optimize the file to use more clearly imported functions

--- Loaders/fetch_obs.py
+++ Loaders/fetch_obs.py
@@ -1,4 +1,4 @@
-#imports from app
+# imports from app
 # from Loaders.Observations import bcb, bcb_exp, cepea, ibge
 # from Loaders.Observations import covid_international, ons
 # from Loaders.Observations import fred, pnad_covid, mobility_apple
@@ -7,40 +7,16 @@ from DB.transactions import fetch_series_list
 
 __all__ = ["fetch_obs"]
 
+
+source_dict = {"BCB":"BCB", "IBGE":"IBGE" , "IPEA":"IPEA", "FRED":"FRED", 
+               "CEPEA":"CEPEA", "COVID_INTERNATIONAL":"COVID_INTERNATIONAL", 
+               "PNAD_COVID":"PNAD_COVID", "APPLE": "APPLE"}
+
+
 def fetch_obs(source: str, limit=None) -> None:
     """fetches and updates (inserts) observations of a particular source
     (ex:BCB) and for last (limit) observations. If limit = None, all
     observations are updated
     """
     Usource = source.upper()
-    if Usource == "BCB":
-        # needs to be implemented
-        return "BCB"
-
-    elif Usource == "IBGE":
-        # needs to be implemented
-        return "IBGE"
-
-    elif Usource == "IPEA":
-        # needs to be implemented
-        return "IPEA"
-
-    elif Usource == "FRED":
-        # needs to be implemented
-        return "FRED"
-
-    elif Usource == "CEPEA":
-        # needs to be implemented
-        return "CEPEA"
-
-    elif Usource == "COVID_INTERNATIONAL":
-        # needs to be implemented
-        return "COVID_INTERNATIONAL"
-
-    elif Usource == "PNAD_COVID":
-        # needs to be implemented
-        return "PNAD_COVID"
-    else:
-        # needs to be implemented: 
-        return "APPLE"
-
+    return source_dict[Usource]
