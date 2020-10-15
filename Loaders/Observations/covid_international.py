7aa077a5438bd10df330775e6eff9e8aee6e12cf
João <jmrosal@JMRosal-Mac-Book-Air.local>
2020-10-15 14:49:06 -0300

    Makes fecth api compliant with app's api and ticker input safe

--- Loaders/Observations/covid_international.py
+++ Loaders/Observations/covid_international.py
@@ -1,44 +1,60 @@
-import concurrent.futures
+#import from system
+from concurrent.futures import ThreadPoolExecutor as executor
+from typing import Optional
+# import from packages
 import requests
 import pandas as pd
 from io import StringIO
+import pendulum
+
+# import from app
 from DB.transactions import add_batch_observations
 
-url_c = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
 
-url_d = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+def build_url(cases:str)->str:
+    """
+    forms the relevant url. case = [confirmed, deaths]
+    """
+    return "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/"+ \
+        "master/csse_covid_19_data/csse_covid_19_time_series/"+  \ 
+        f"time_series_covid19_{cases}_global.csv"
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-    futs = executor.map(lambda url: requests.get(url), [url_c, url_d])
-
-dresps = [pd.read_csv(StringIO(f.text)) for f in futs]    
-
-dfs = [(df.groupby("Country/Region").sum().drop(columns=["Lat","Long"])).T
-           for df in dresps]
-
-for df in dfs:
+def _process(resp:requests.models.Response)-> pd.DataFrame:
+    """
+    process the response of a request coming from the disired url
+    a return a dataframe
+    """
+    dr = pd.read_csv(StringIO(resp.text))
+    df =  (dr.groupby("Country/Region").sum().drop(columns=["Lat","Long"])).T
     df.index = pd.to_datetime(df.index, dayfirst=False)
+    types = "cases" if "confirmed" in resp.url else "fatalities"
+    df.columns = [f"JHU.{c.replace(' ','_')}_{types}".upper() 
+                  for c in df.columns]
+    return df
 
-countries = ['Italy', 'United Kingdom', 'US', 'Germany',
-             'France', 'Brazil', 'China', "Spain"]
 
-# cases:
-for c in countries:
-    tck = f"JHU.{c.replace(' ','_')}_cases".upper()
-    try:
-        add_batch_observations(tck, dfs[0].loc[:, [c]])
-        print(f"Series {tck} sucessfully added!")
+def fetch(tickers:str, limit:Optional[int] = 10):
+    """
+    upsert the observations of tickers into the database. If limit is None
+    to that for the whole set of observations; if limit is int, does that
+    only for the latest limit observation
+    """
+    urls = [build_url(cases) for cases in ["confirmed", "deaths"]]
+    
+    with requests.session() as session:
+        with executor(max_workers=2) as e:
+            resps = e.map(lambda url: session.get(url), urls)
 
-    except:
-        print(f"Series {tck} failed to be added!")
+    dfs = [_process(r) for r in resps]
+    df = dfs[0].merge(dfs[1], left_index=True, right_index=True, how="inner")
+    df = df if limit is None else df.tail(limit)
 
-# fatalities:
-for c in countries:
-    tck = f"JHU.{c.replace(' ','_')}_fatalities".upper()
-    try:
-        add_batch_observations(tck, dfs[1].loc[:, [c]])
-        print(f"Series {tck} sucessfully added!")
-    except:
-        print(f"Series {tck} failed to be added!")
+    with executor() as e1:
+        dz = [(tck, df.loc[:, [tck]]) for tck in tickers]
+        e1.map(lambda z: add_batch_observations(*z), dz)
+
+    return {"source": "JHU", 
+            "time": pendulum.now().to_datetime_string(),
+            "limit": limit}
 
 
